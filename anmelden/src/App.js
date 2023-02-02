ae6887673226dd0349f133f9f5866da9d1e73a45
ArAjAlGro <armin.grossmann@sourcetronic.com>
2023-02-02 17:44:24 +0100

    #

--- anmelden/src/App.js
+++ anmelden/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Sidebar from './Sidebar';
 import Test from './test'
 
 function App() {
   return (
-    <div className="App">
-      <Test/>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Sidebar/>
+      yo
     </div>
   );
 }
