1d5f45fcab239ecae0f2a88c45bf7a1010e905dd
ArAjAlGro <armin.grossmann@sourcetronic.com>
2023-02-10 17:24:17 +0100

    #

--- anmelden/src/App.js
+++ anmelden/src/App.js
@@ -1,12 +1,109 @@
-import './App.css';
-import Sidebar from './Sidebar';
-import Test from './test'
+import "./App.css";
+import Bg from "./Bg";
+import Sidebar from "./Sidebar";
+import Test from "./test";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Sidebar/>
-      yo
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" class="px-6 py-3">
+              Datum und Uhrzeit
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Wasserstand FB
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Wasserstand SB
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Temp. FB
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Temp. SB
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Temp. Sole Eintritt
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Temp. Sole Austritt
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Temp. Delta
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Zählerstand
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Förderrate
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+            <td class="px-6 py-4">2021-04-21 16:07:04.999</td>
+            <td class="px-6 py-4">9,151</td>
+            <td class="px-6 py-4">9,173</td>
+            <td class="px-6 py-4">13,500</td>
+            <td class="px-6 py-4">12,600</td>
+            <td class="px-6 py-4">13,500</td>
+            <td class="px-6 py-4">13,500</td>
+            <td class="px-6 py-4">0,900</td>
+            <td class="px-6 py-4">1896,138</td>
+            <td class="px-6 py-4">3,8</td>
+          </tr>
+          <tr class="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+            <td class="px-6 py-4">2021-04-21 16:07:04.999</td>
+            <td class="px-6 py-4">9,151</td>
+            <td class="px-6 py-4">9,173</td>
+            <td class="px-6 py-4">13,500</td>
+            <td class="px-6 py-4">12,600</td>
+            <td class="px-6 py-4">13,500</td>
+            <td class="px-6 py-4">13,500</td>
+            <td class="px-6 py-4">0,900</td>
+            <td class="px-6 py-4">1896,138</td>
+            <td class="px-6 py-4">3,8</td>
+          </tr>
+          <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+            <td class="px-6 py-4">2021-04-21 16:07:04.999</td>
+            <td class="px-6 py-4">9,151</td>
+            <td class="px-6 py-4">9,173</td>
+            <td class="px-6 py-4">13,500</td>
+            <td class="px-6 py-4">12,600</td>
+            <td class="px-6 py-4">13,500</td>
+            <td class="px-6 py-4">13,500</td>
+            <td class="px-6 py-4">0,900</td>
+            <td class="px-6 py-4">1896,138</td>
+            <td class="px-6 py-4">3,8</td>
+          </tr>
+          <tr class="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+            <td class="px-6 py-4">2021-04-21 16:07:04.999</td>
+            <td class="px-6 py-4">9,151</td>
+            <td class="px-6 py-4">9,173</td>
+            <td class="px-6 py-4">13,500</td>
+            <td class="px-6 py-4">12,600</td>
+            <td class="px-6 py-4">13,500</td>
+            <td class="px-6 py-4">13,500</td>
+            <td class="px-6 py-4">0,900</td>
+            <td class="px-6 py-4">1896,138</td>
+            <td class="px-6 py-4">3,8</td>
+          </tr>
+          <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+            <td class="px-6 py-4">2021-04-21 16:07:04.999</td>
+            <td class="px-6 py-4">9,151</td>
+            <td class="px-6 py-4">9,173</td>
+            <td class="px-6 py-4">13,500</td>
+            <td class="px-6 py-4">12,600</td>
+            <td class="px-6 py-4">13,500</td>
+            <td class="px-6 py-4">13,500</td>
+            <td class="px-6 py-4">0,900</td>
+            <td class="px-6 py-4">1896,138</td>
+            <td class="px-6 py-4">3,8</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
---
+++ anmelden/src/Bg.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Bg = () => {
+  return (
+    <div className='bg-gray-400 '>Bg</div>
+  )
+}
+
+export default Bg
